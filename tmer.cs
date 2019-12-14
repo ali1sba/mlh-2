55c3f93907abc3374a4d98a3d0f993a8bb30b4eb
ali1sba <hariri.ali.official@gmail.com>
2019-12-14 16:29:16 +0100

    1 update

---
+++ tmer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class tmer : MonoBehaviour
+{
+     [SerializeField]private Text uitext;
+     [SerializeField]private float mainTimer;
+     private float timer;
+     private bool canCount=true;
+     private bool doOnce=false;
+       void Start()
+    {
+        timer=mainTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer>= 0.0f && canCount){
+            timer -= Time.deltaTime;
+            uitext.text =timer.ToString("F");
+        }
+        else if (timer<=0.0f && !doOnce){
+            canCount= false;
+            doOnce=true;
+            uitext.text="0.00";
+            timer=0.0f;
+        }
+    }
+}
